a587a2e24bdd2243aa0b05b393f1ab5698706069
B H S khan <bhskhan24@gmail.com>
2026-09-19 01:40:27 +0600

    Fix: Added missing SharedPreferences and intl imports in subscription_screen

--- lib/screens/subscription_screen.dart
+++ lib/screens/subscription_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 import '../utils/notification_utils.dart';
 import 'subscription_receipt_screen.dart';
 import '../utils/translations.dart';
