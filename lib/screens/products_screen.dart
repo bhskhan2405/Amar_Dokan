796af2a6e41c9af9334b9674f4f6685e1eeb44e4
B H S khan <bhskhan24@gmail.com>
2026-09-19 23:39:35 +0600

    Fix: Added missing intl import in products_screen

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
